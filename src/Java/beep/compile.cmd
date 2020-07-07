6b89247ce1a05b198013b323d0ca7dda817559de
Oded <oded.nissan@gmail.com>
2020-07-07 15:45:36 +0300

    remove

--- src/Java/beep/compile.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-gcc -c NativeUtils.c
-gcc -shared -o nativeutils.dll NativeUtils.o
--- src/Java/beep/sp_beep.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <windows.h>
-
-int main(int argc, char **argv)
-{
-	printf("in sp_beep\n");
-	Beep(500,1000);
-	return(0);
-}
